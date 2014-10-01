894107a928545b5067f5edb2efc2337fdd19608a
Joshua Karstendick <joshdick@gmail.com>
2014-10-01 13:24:55 -0400

    more work on logic

--- src/minesweeper_clone/logic.clj
+++ src/minesweeper_clone/logic.clj
@@ -22,37 +22,54 @@
     (let [m (first maskv)]
       (all
         (if-not (= "H" m)
-            (== (first vars) m)
+          (== (first vars) m)
           succeed)
         (match-mask (next vars) (next maskv))))
     succeed))
 
-(defn run-logic
-  []
-  (let [[num-rows num-cols num-mines] [2 2 2]
-        num-cells (total-cells num-rows num-cols)
-        boardv [2 "M" 2 "M"]
-        board (vector->board boardv num-rows num-cols)
-        maskv ["H" "H" 2 "H"]
-        mask (vector->board maskv num-rows num-cols)
-        [r c] [1 1]
+;; TODO: don't generate lvars for revealed cells (or correctly flagged ones?)
+
+(defn gen-matching-boards
+  [[num-rows num-cols num-mines] [r c] mask]
+  (let [num-cells (total-cells num-rows num-cols)
+        maskv (flatten mask)
         vars (repeatedly num-cells lvar)
         vars-board (vector->board vars num-rows num-cols)
-        ;_ (println vars)
         ]
-    (run* [q] ;;try run*
-         (== q vars)
-         (everyg #(membero % [0 1 2 3 "M"]) vars)
-         (membero (get-in vars-board [r c]) [0 1 2 3])
-         (match-mask vars maskv)
-         (project [vars]
-                  (let [vars-board (vector->board vars num-rows num-cols)]
-                    (== num-mines (count (filter #{"M"} vars)))
-                    (== vars-board
-                        (calc-board num-rows num-cols vars-board))
-                    ))
-         ;(everyg #(membero % [0 "M"]) vars)
-         ;(== num-mines (count (filter #(== % 9) vars)))
-         ;;(membero 9)
+    (run* [q] ;;try run* or run 1
+          (== q vars)
+          (everyg #(membero % [0 1 2 3 4 5 6 7 8 "M"]) vars)
+          (match-mask vars maskv)
+          (!= (get-in vars-board [r c]) "M")
+          (project [vars]
+                   (let [vars-board (vector->board vars num-rows num-cols)]
+                     (== num-mines (count (filter #{"M"} vars)))
+                     (== vars-board
+                         (calc-board num-rows num-cols vars-board))
+                     ))
+          )))
 
-         )))
+(defn run-logic
+  []
+  (gen-matching-boards [2 2 2]
+                       [1 1]
+                       [["H" "H"]
+                        [2 "H"]])
+  ; (gen-matching-boards [3 3 3]
+  ;                      ;[2 2]
+  ;                      [0 0]
+  ;                      [[1 "H" "H"]
+  ;                       [1 3 3]
+  ;                       ["H" "H" "H"]])
+  )
+
+
+
+
+
+
+
+
+
+
+
